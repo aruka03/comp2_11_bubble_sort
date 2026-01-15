d0cd80f360ccd87385c1a09d1c97ba884b1b2a9b
aruka03 <sakura1911121sakumondou@gmail.com>
2026-01-16 03:18:10 +0900

    テスト通りました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,17 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL || end < begin)
+		return false;
+
+	for (item* i = begin; i < end-1; i++) {//1週ごとに大きい数が右に固定
+		for (item* j = begin; j < end-1-(i - begin); j++) {//隣と比べる
+			if (j->key > (j + 1)->key) {//ここで交換
+				item tmp = *j;
+				*j = *(j + 1);
+				*(j + 1) = tmp;
+			}
+		}
+	}
+	return true;
 }
